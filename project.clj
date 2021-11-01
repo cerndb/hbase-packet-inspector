a9004964c7f1b359c26f105f34214769e4536947
Ignacio Coterillo <ignacio.coterillo@cern.ch>
2021-11-01 11:10:44 +0100

    wip: Update dependencies

--- project.clj
+++ project.clj
@@ -1,26 +1,27 @@
-(defproject hbase-packet-inspector "0.2.0"
+(defproject hbase-packet-inspector "0.3.0"
   :description "A command-line tool for analyzing network traffic of HBase RegionServers"
   :url "http://github.com/kakao/hbase-packet-inspector"
   :license {:name "Apache License 2.0"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/core.async "0.3.443"]
-                 [org.clojure/core.match "0.3.0-alpha4"]
-                 [org.clojure/tools.cli "0.3.5"]
-                 [org.clojure/tools.logging "0.4.0"]
-                 [org.clojure/java.jdbc "0.7.1"]
-                 [org.slf4j/slf4j-api "1.7.25"]
-                 [org.slf4j/slf4j-log4j12 "1.7.25"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.clojure/core.async "1.3.622"]
+                 [org.clojure/core.match "1.0.0"] 
+                 [org.clojure/tools.cli "1.0.206"]
+                 [org.clojure/tools.logging "1.1.0"]
+                 [org.clojure/java.jdbc "0.7.12"]
+                 [org.slf4j/slf4j-api "1.7.32"]
+                 [org.slf4j/slf4j-log4j12 "1.7.32"]
                  [org.apache.hbase/hbase-client "1.2.6"]
-                 [org.pcap4j/pcap4j-core "1.7.1"]
-                 [org.pcap4j/pcap4j-packetfactory-static "1.7.1"]
-                 [com.google.guava/guava "19.0"]
-                 [com.google.protobuf/protobuf-java "2.5.0"]
-                 [com.h2database/h2 "1.4.196"]
-                 [org.apache.kafka/kafka-clients "0.11.0.0"]
-                 [cheshire "5.8.0"]
+                 ;[org.apache.hbase/hbase-client "2.4.7"] ; warnings and suggestions
+                 [org.pcap4j/pcap4j-core "1.8.2"]
+                 [org.pcap4j/pcap4j-packetfactory-static "1.8.2"]
+                 [com.google.guava/guava "31.0.1-jre"]
+                 [com.google.protobuf/protobuf-java "2.6.1"] ; Issues with v3
+                 [com.h2database/h2 "1.4.197"] ; v1.4.198: 1 error
+                 [org.apache.kafka/kafka-clients "3.0.0"]
+                 [cheshire "5.10.0"]
                  [com.cemerick/url "0.1.1"]
                  [junegunn/grouper "0.1.1"]]
-  :plugins [[lein-cloverage "1.0.9"]]
+  :plugins [[lein-cloverage "1.2.2"]]
   :main hbase-packet-inspector.core
   :target-path "target/%s"
   :uberjar-name "../hbase-packet-inspector-%s.jar"
